159f208757ac3b49738f513c7c8e548dd6043a94
TWT-Kodision <107999822+TWT-Kodision@users.noreply.github.com>
2022-10-28 15:19:19 +0200

    Add files via upload
    Module 2: Arguments

--- main.py
+++ main.py
@@ -1,24 +1,31 @@
 # Do not modify these lines
-__winc_id__ = '71dd124b4a6e4d268f5973db521394ee'
-__human_name__ = 'strings'
+__winc_id__ = '7b9401ad7f544be2a23321292dd61cb6'
+__human_name__ = 'arguments'
 
 # Add your code after this line
-first_goal = "Ruud Gullit"
-second_goal = "Marco van Basten"
-goal_0 = 32
-goal_1 = 54
-scorers = first_goal+f' {goal_0}'+", "+ second_goal+f' {goal_1}'
-report = f'{first_goal} scored in the {goal_0}nd minute\n{second_goal} scored in the {goal_1}th minute'
-print (scorers)
-print(report)
-player = "Hans van Breukelen"
-first_name = player[0:player.find(" ")]
-print(first_name)
-last_name_len = len(player[player.find(" ")+1:])
-print(last_name_len)
-name_short = player[0] + ". " + player[player.find(" ")+1:]
-print(name_short)
-chant = (first_name + "! ") * (len(first_name)-1) + (first_name + "!")
-print(chant)
-good_chant = chant[len(chant)-1]!=" "
-print(good_chant)
+def greet(name, greeting = 'Hello, <name>!'):
+    new_greeting = greeting.replace('<name>', name)
+    return (new_greeting)
+    
+def force(mass, body = 'earth'):
+    body = body.lower()
+    gravitys = {
+        'sun': 274,
+        'jupiter': 24.9,
+        'neptune': 11.2,
+        'saturn': 10.4,
+        'earth': 9.8,
+        'uranus': 8.9,
+        'veus': 8.9,
+        'mars': 3.7,
+        'mercury': 3.7,
+        'moon': 1.6,
+        'pluto': 0.6
+        }
+    body_force = mass * gravitys[body]
+    return body_force
+
+def pull(m1, m2, d):
+    G = 6.674*10**-11
+    gravitational_pull =  G*(m1*m2)/d**2
+    return gravitational_pull

c3c3866d3a623f047347f05e7ccc96d1e84df95b
TWT-Kodision <107999822+TWT-Kodision@users.noreply.github.com>
2022-11-11 18:40:56 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,31 +1,49 @@
-# Do not modify these lines
-__winc_id__ = '7b9401ad7f544be2a23321292dd61cb6'
-__human_name__ = 'arguments'
+__winc_id__ = "ae539110d03e49ea8738fd413ac44ba8"
+__human_name__ = "files"
 
-# Add your code after this line
-def greet(name, greeting = 'Hello, <name>!'):
-    new_greeting = greeting.replace('<name>', name)
-    return (new_greeting)
-    
-def force(mass, body = 'earth'):
-    body = body.lower()
-    gravitys = {
-        'sun': 274,
-        'jupiter': 24.9,
-        'neptune': 11.2,
-        'saturn': 10.4,
-        'earth': 9.8,
-        'uranus': 8.9,
-        'veus': 8.9,
-        'mars': 3.7,
-        'mercury': 3.7,
-        'moon': 1.6,
-        'pluto': 0.6
-        }
-    body_force = mass * gravitys[body]
-    return body_force
+import os
+from zipfile import ZipFile
 
-def pull(m1, m2, d):
-    G = 6.674*10**-11
-    gravitational_pull =  G*(m1*m2)/d**2
-    return gravitational_pull
+#removes files from dir
+def clear_dir(path):
+    for file_name in os.listdir(path):
+        os.remove(f'{path}/{file_name}') #constructs path and remove the file
+
+#makes dir or cleans it
+def clean_cache():
+    dir = 'files/cache'
+    if os.path.isdir(dir):  #check if dir exist
+        clear_dir(dir)
+    else:
+        os.mkdir(dir)
+
+#extract zip and place in cache
+def cache_zip(zip_file, cache_path):
+    zip_object = ZipFile(zip_file)
+    zip_object.extractall(cache_path)
+
+#put data to cache
+def data_to_cache():
+    zip_file = 'files/data.zip'
+    cache_path = 'files/cache'
+    cache_zip(zip_file, cache_path)
+
+#makes list of cached files
+def cached_files():
+    cache_path = os.path.abspath("files\cache")
+    cache_list = []
+    for file_name in os.listdir(cache_path):
+        cache_list.append(f'{cache_path}\{file_name}')
+    return cache_list
+
+#find password in file list
+def find_password(file_list):
+    password_indication = 'password'
+    for file in file_list:
+        file_content = open(file)
+        content = file_content.read()
+        if password_indication in content:
+            password_raw = content[content.find(password_indication)+len(password_indication)+2:] #remove string before password
+            password = password_raw[0:password_raw.find('\n')]  #remove lines after password
+            return password
+        file_content.close

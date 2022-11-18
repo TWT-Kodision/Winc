78fc4eb05e74800903097c72a3fa6356de4111d9
TWT <t.tu@kodision.nl>
2022-11-18 16:42:37 +0100

    module3 files

--- module3/files/main.py
+++ module3/files/main.py
@@ -5,20 +5,19 @@ import os
 from zipfile import ZipFile
 
 #get current dir and join path
-def get_current_dir(folder):
-    current_dir = os.path.join(os.getcwd(), folder)
+def get_current_dir(folder = 'cache'):
+    current_dir = os.path.join(os.getcwd(), 'files', folder)
     return current_dir
 
 
 #removes files from dir
 def clear_dir(path):
     for file_name in os.listdir(path):
-        os.remove(f'{path}/{file_name}') #constructs path and removes the file
+        os.remove(os.path.join(path, file_name)) #constructs path and removes the file
 
 #makes dir or cleans it
 def clean_cache():
-    dir = get_current_dir('module3/files/cache')
-    print (f'the dir is {dir}')
+    dir = get_current_dir()
     if os.path.isdir(dir): #check if dir exist
         clear_dir(dir)
     else:
@@ -31,20 +30,20 @@ def cache_zip(zip_file, cache_path):
 
 #put data to cache
 def data_to_cache():
-    zip_file = get_current_dir('files/data.zip')
-    cache_path = get_current_dir('files/cache')
+    zip_file = get_current_dir('data.zip')
+    cache_path = get_current_dir()
     cache_zip(zip_file, cache_path)
 
 #makes list of cached files
 def cached_files():
-    cache_path = os.path.abspath('files/cache')
+    cache_path = get_current_dir()
     cache_list = []
     for file_name in os.listdir(cache_path):
-        cache_list.append(f'{cache_path}/{file_name}')
+        cache_list.append(os.path.join(cache_path, file_name))
     return cache_list
 
-#find password in file list
-def find_password(file_list):
+#find password in file list using a list
+def find_password(file_list = cached_files()):
     password_indication = 'password'
     for file in file_list:
         file_content = open(file)
@@ -54,6 +53,3 @@ def find_password(file_list):
             password = password_raw[0:password_raw.find('\n')]  #remove lines after password
             return password
         file_content.close
-clean_cache()
-
-

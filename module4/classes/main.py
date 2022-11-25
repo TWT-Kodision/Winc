3dcb249b7d482a90f51c3856fd0f3ea7ccda02c6
TWT <t.tu@kodision.nl>
2022-11-25 15:28:54 +0100

    progress save

---
+++ module4/classes/main.py
@@ -0,0 +1,13 @@
+# Do not modify these lines
+__winc_id__ = '04da020dedb24d42adf41382a231b1ed'
+__human_name__ = 'classes'
+
+# Add your code after this line
+class Player():
+    def __init__(self, name, speed, endurance, accuracy):
+        try:
+            speed 
+        self.name = name
+        self.speed = speed
+        self.endurance = endurance
+        self.accuracy = accuracy

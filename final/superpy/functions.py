0bebd3bd2816e4a33804b29a546445559cc26a7c
TWT <t.tu@kodision.nl>
2023-02-20 13:57:33 +0100

    backup

--- final/superpy/functions.py
+++ final/superpy/functions.py
@@ -1,3 +1,25 @@
+from csvController import*
+
+def c
+
+def checkProductInput(inputList):
+
+def registerBoughtProduct(id, product, product_name, buy_date, expiration_date, amount):
+    addToBought(id, product, product_name, buy_date, expiration_date, amount)
+
+
+
+
+
+#product list
+#product list with number/count
+#product bought price and expiry date
+#product sold price
+#expired products
+#set date to today or future date
+
+
+
 #calculate inventory
 #check for expriration date
 #register bought product
@@ -5,4 +27,18 @@
 #get bought id
 #get sold id
 #set dateformat
-
+"""     
+    Which products the supermarket offers;
+    How many of each type of product the supermarket holds currently;
+    How much each product was bought for, and what its expiry date is;
+    How much each product was sold for or if it expired, the fact that it did;
+    
+    Setting and advancing the date that the application perceives as 'today';
+    Recording the buying and selling of products on certain dates;
+    Reporting revenue and profit over specified time periods;
+    Exporting selections of data to CSV files;
+    Two other additional non-trivial features of your choice, for example:
+        The use of an external module Rich(opens in a new tab) to improve the application.
+        The ability to import/export reports from/to formats other than CSV (in addition to CSV)
+        The ability to visualize some statistics using Matplotlib(opens in a new tab)
+        Another feature that you thought of.  """

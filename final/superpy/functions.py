c7b26898bdd5b93414f66dd746debfb186d60e70
TWT <t.tu@kodision.nl>
2023-03-07 21:57:21 +0100

    update

--- final/superpy/functions.py
+++ final/superpy/functions.py
@@ -1,14 +1,111 @@
 from csvController import*
+from datetime import datetime 
 
-def c
+def makeNewCSVFile(filename, column_name=None):
+    try:
+        if filename == "bought.csv":
+            makeBoughtFile()
+        elif filename == "sold.csv":
+            makeSoldFile()
+        else:
+            makeFile(filename, column_name)
+    except ValueError:
+        print("filename or column name not correct")
 
 def checkProductInput(inputList):
+    return None
 
-def registerBoughtProduct(id, product, product_name, buy_date, expiration_date, amount):
-    addToBought(id, product, product_name, buy_date, expiration_date, amount)
+def convertToDateObject(date):
+    date_object = datetime.strptime(date, '%Y-%m-%d').date()
+    return date_object
+
+def registerBoughtProduct(id, product, product_name, buy_date, expiration_date):
+    addToBought(id, product, product_name, buy_date, expiration_date)
+
+def registerSoldProduct(id, bought_id, sell_date, sell_price):
+    addToSold(id, bought_id, sell_date, sell_price)
+
+def inventoryList():
+    sold_list = getDataList("sold.csv")
+    inventory_list = getDataList("bought.csv")
+    for sold_item in sold_list:
+        for bought_item in inventory_list:
+            if sold_item[1] == bought_item [0]:
+                sold_list.remove(sold_item)
+                inventory_list.remove(bought_item)
+    return inventory_list
+
+def getProductDetails(search_product):
+    inventory_list = inventoryList()
+    product_details = []
+    for product in inventory_list:
+        if product[1] == search_product:
+            product_details.append(product)
+    return product_details
+
+def getExpiredProductList(day = date.today()):
+    inventory_list = inventoryList()
+    del inventory_list[0] #remove header
+    expired_products = []
+    for product in inventory_list:
+        date_object = convertToDateObject(product[4])
+        if (date_object) <= day:
+            expired_products.append(product)
+    return expired_products
+
+def getSoldProductList():
+    sold_products = []
+    return sold_products
+
+def findProductById(id):
+    bought_list = getDataList("bought.csv")
+    for product in bought_list:
+        if product[0]==id:
+            return product
+        
+def getProductsDateRange(begin_date, end_date, bought_or_sold):
+    begin_date_object = convertToDateObject(begin_date)
+    end_date_object = convertToDateObject(end_date)
+    list_range = []
+    if bought_or_sold == "bought":
+        product_list = getDataList("bough.csv")
+    if bought_or_sold == "sold":
+        product_list = getDataList("sold.csv")
+    del product_list[0] #remove header
+    for product in product_list:
+        product_date_object = convertToDateObject(product[2])
+        if product_date_object >= begin_date_object and product_date_object <= end_date_object:
+            list_range.append(product)
+    return list_range
 
 
+def calculateProfit(begin_date, end_date):
+    #make sold list for specified range
+    sold_list_range = getProductsDateRange(begin_date, end_date, "sold")
+    profit = 0
+    for product in sold_list_range:
+        print(product)
+        product_bought_details = findProductById(product[1])
+        print(product_bought_details)
+        product_profit =  float(product[3])-float(product_bought_details[3])
+        profit += product_profit
+    return profit  
 
+def countEachProductInventory():
+    inventory_list = inventoryList()
+    counts = dict()
+    del inventory_list[0]
+    for product in inventory_list:
+        counter = 0
+        for compare_product in inventory_list:
+            if product[1]==compare_product[1]:
+                counter += 1
+        counts[product[1]] = counter
+    return(counts)
+
+def plotGraphSoldPerDay(begin_date, end_date):
+    
+            
 
 
 #product list
@@ -17,16 +114,11 @@ def registerBoughtProduct(id, product, product_name, buy_date, expiration_date, 
 #product sold price
 #expired products
 #set date to today or future date
+#list of bought and sold products at certain date
+#profit time for specific time period
+#export selection data to csv file 
 
 
-
-#calculate inventory
-#check for expriration date
-#register bought product
-#register sold product
-#get bought id
-#get sold id
-#set dateformat
 """     
     Which products the supermarket offers;
     How many of each type of product the supermarket holds currently;

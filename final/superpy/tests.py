8276394874fcf5778c4c13f38ab9c6b6992cd6b5
TWT <t.tu@kodision.nl>
2023-03-07 22:13:42 +0100

    update

---
+++ final/superpy/tests.py
@@ -0,0 +1,46 @@
+from csvController import*
+from functions import*
+
+
+
+makeBoughtFile()
+makeSoldFile()
+addToBought(12, "name", date(2022, 12, 14), 15, date(2023, 12, 15))
+addToBought(22, "name2", date(2023, 1, 22), 16, date(2023, 1, 2))
+addToBought(23, "name3", date(2023, 1, 23), 17, date(2023, 1, 3))
+addToBought(24, "name4", date(2023, 1, 24), 18, date(2023, 2, 4))
+addToBought(25, "name2", date(2023, 1, 25), 19, date(2023, 6, 5))
+addToBought(26, "name6", date(2023, 1, 26), 11, date(2023, 2, 6))
+addToBought(27, "name6", date(2023, 1, 27), 12, date(2023, 6, 7))
+addToBought(28, "name8", date(2023, 1, 28), 13, date(2023, 6, 8))
+addToBought(29, "name6", date(2023, 1, 29), 14, date(2023, 6, 9))
+
+addToSold(1265, 12, date(2023, 2, 14), 24.50)
+addToSold(2223, 22, date(2023, 3, 22), 26.50)
+addToSold(2323, 24, date(2023, 2, 22), 27.50)
+addToSold(2423, 27, date(2023, 3, 22), 23.50)
+#dataList = getDataList("bought.csv")
+#searchList = ["12", "test", "name", "2022-12-14", "2023-12-15"]
+
+#print(dataList[1])
+#print(searchList)
+#print(getDataList("bought.csv"))
+#print(getDataList("sold.csv"))
+#print(dataList[1] == searchList)
+#print(getRowIndex(dataList, searchList))
+
+
+
+
+#print(getBoughtListByBoughtId("12"))
+#print(getSoldListByBoughtId("12"))
+#print(int(getBoughtListByBoughtId("12")[0][5]) - int(getSoldListByBoughtId("12")[0][4]))
+
+
+
+#print('list is')
+#print(inventoryList())
+#print(getProductDetails("name2"))
+print(getExpiredProductList())
+print(calculateProfit("2023-02-01", "2023-2-28"))
+print(countEachProductInventory())

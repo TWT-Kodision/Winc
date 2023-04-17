6918e1ed54b49e26fbeee29efb766c7a6be46d16
TWT <t.tu@kodision.nl>
2023-04-17 14:47:44 +0200

    module6

--- module6/authentication/helpers.py
+++ module6/authentication/helpers.py
@@ -9,8 +9,21 @@ def get_users():
     return {
         "Alice": "b778a39a3663719dfc5e48c9d78431b1e45c2af9df538782bf199c189dabeac7680ada57dcec8eee91c4e3bf3bfa9af6ffde90cd1d249d1c6121d7b759a001b1",
         "Bob": "d07e870dbf957e794c53fe56b0940d75c6b752096f59a90aaa31b4a420593fc5c520a2f51e884c34756e2445cfe2edce86ae566d22031bd02390a18c0fe0a63b",
+        "Test": "9ece086e9bac491fac5c1d1046ca11d737b92a2b2ebd93f005d7b710110c0a678288166e7fbe796883a4f2e9b3ca9f484f521d0ce464345cc1aec96779149c14",
     }
 
 
 def hash_password(password):
     return sha3_512(password.encode("utf-8")).hexdigest()
+
+
+def valid_password(username, password):
+    users = get_users()
+    hashed_password = hash_password(password)
+    try:
+        if users[username] == hashed_password:
+            return True
+        else:
+            return False
+    except KeyError:
+        return False
--- module6/authentication/main.py
+++ module6/authentication/main.py
@@ -1,8 +1,7 @@
 import os
 
 from flask import Flask, redirect, render_template, request, session, url_for
-from helpers import get_users, hash_password
-
+from helpers import valid_password
 __winc_id__ = "8fd255f5fe5e40dcb1995184eaa26116"
 __human_name__ = "authentication"
 
@@ -34,20 +33,24 @@ def lon():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == 'POST':
-        session['username'] = request.form['username']
-        return redirect(url_for('index'))
+        if valid_password(request.form['username'], request.form['password']):
+            session['logged'] = True
+            session['username']= request.form['username']
+            return redirect(url_for('dashboard'))
+        return redirect(url_for('login', error=True))
     return render_template("login.html", title="login")
-
+      
 
 @app.route("/dashboard")
 def dashboard():
-    # YOUR SOLUTION HERE
-    pass
+    return render_template("dashboard.html", title="dashboard")
 
 
 @app.route("/logout", methods=["GET", "POST"])
 def logout():
-    def logout():
-        session.pop('username', None)
-        return redirect(url_for('index'))
-    pass
+    session['logged'] = False
+    session.pop('username', None)
+    return redirect(url_for('index'))
+
+
+app.run
